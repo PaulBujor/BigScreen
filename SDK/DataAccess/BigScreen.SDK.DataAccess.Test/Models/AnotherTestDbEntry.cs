65f045b27372b487794412eddacdaef000f3a48a
Paul-Andrei Bujor <paul.andrei.bujor@outlook.com>
2022-05-15 00:20:57 +0200

    DbContainerAttribute created

--- SDK/DataAccess/BigScreen.SDK.DataAccess.Test/Models/AnotherTestDbEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿using BigScreen.SDK.DataAccess.Core;
-using Newtonsoft.Json;
-
-namespace BigScreen.SDK.DataAccess.Test.Models;
-
-public class AnotherTestDbEntry : BaseDbEntry
-{
-    [JsonProperty("AnotherEntryName")] public string? AnotherEntryName { get; set; }
-}
---
+++ SDK/DataAccess/BigScreen.SDK.DataAccess.Test/Models/DbContainerAttributeTest/CorrectDbEntry.cs
@@ -0,0 +1,11 @@
+﻿using BigScreen.SDK.DataAccess.Attributes;
+using BigScreen.SDK.DataAccess.Core;
+using Newtonsoft.Json;
+
+namespace BigScreen.SDK.DataAccess.Test.Models.DbContainerAttributeTest;
+
+[DbContainer(ContainerName = "Correct", PartitionKey = "somePartition")]
+public class CorrectDbEntry : BaseDbEntry
+{
+    [JsonProperty("somePartition")] public string? SomePartition { get; set; }
+}
