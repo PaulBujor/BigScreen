deb996322a801accc9e997be89fee0700337735d
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2022-05-21 13:04:37 +0200

    cleanup

--- Core/BigScreen.Core/Models/TMDb/MovieDto.cs
+++ Core/BigScreen.Core/Models/TMDb/MovieDto.cs
@@ -7,19 +7,19 @@ namespace BigScreen.Core.Models.TMDb;
 [TmdbDto("movie")]
 public class MovieDto : TmdbDto
 {
-    [JsonProperty(PropertyName = "id")]
-    public int Id { get; set; }
+    [JsonProperty(PropertyName = "id")] public int Id { get; set; }
 
-    [JsonProperty(PropertyName = "title")]
-    public string Title { get; set; }
+    [JsonProperty(PropertyName = "title")] public string Title { get; set; }
 
     [JsonProperty(PropertyName = "release_date")]
     public DateTime ReleaseDate { get; set; }
 
     [JsonProperty(PropertyName = "revenue")]
     public long Revenue { get; set; }
+
     [JsonProperty(PropertyName = "budget")]
     public long Budget { get; set; }
+
     [JsonProperty(PropertyName = "vote_average")]
     public double TmdbScore { get; set; }
 
