96115bc706bc21d172664801ccc2c3ddb73e6536
AlexandruTatarciuc <tatarciucalexandru@gmail.com>
2022-05-21 13:21:09 +0200

    cleanup

--- Core/BigScreen.Core/Models/TMDb/GenreDto.cs
+++ Core/BigScreen.Core/Models/TMDb/GenreDto.cs
@@ -3,12 +3,11 @@ using BigScreen.Frontend.Core.Attributes;
 using Newtonsoft.Json;
 
 namespace BigScreen.Core.Models.TMDb;
+
 [TmdbDto("genre")]
 public class GenreDto : TmdbDto
 {
-    [JsonProperty(PropertyName = "id")]
-    public int Id { get; set; }
+    [JsonProperty(PropertyName = "id")] public int Id { get; set; }
 
-    [JsonProperty(PropertyName = "name")]
-    public string Name { get; set; }
+    [JsonProperty(PropertyName = "name")] public string Name { get; set; }
 }
