291d563d3667889da4a0df0b2212db24be706863
Aurimas Bruzgys <55162517+Ranteidz@users.noreply.github.com>
2022-05-29 23:12:07 +0200

    format

--- Frontend/BigScreen.Frontend.Client/Constants/BigScreenClientConstants.cs
+++ Frontend/BigScreen.Frontend.Client/Constants/BigScreenClientConstants.cs
@@ -3,13 +3,13 @@ namespace BigScreen.Frontend.Client.Constants;
 public static class BigScreenClientConstants
 {
     public const string ClientName = "BigScreenClient";
-    private static string _baseAddress = "https://bigscreen-api.azurewebsites.net/api";
-    private static readonly string _localAdress = "https://localhost:7171/api";
+    private const string BaseAddress = "https://bigscreen-api.azurewebsites.net/api";
+    private const  string LocalAddress = "https://localhost:7171/api";
 
     public static string GetBaseAddress()
     {
 #if DEBUG
-        return _localAdress;
+        return LocalAddress;
 #else
         return BaseAddress;
 #endif
